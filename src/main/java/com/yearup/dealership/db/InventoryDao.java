e98b2489d21ba0f2e07b0ad716463c5fb3f71547
tenzin <niznetgaming@gmail.com>
2023-06-16 11:46:08 -0400

    added logic to add vehicles to the inventory.

--- src/main/java/com/yearup/dealership/db/InventoryDao.java
+++ src/main/java/com/yearup/dealership/db/InventoryDao.java
@@ -13,7 +13,19 @@ public class InventoryDao {
     }
 
     public void addVehicleToInventory(String vin, int dealershipId) {
-        // TODO: Implement the logic to add a vehicle to the inventory
+        // Implement the logic to add a vehicle to the inventory
+        String query = "SELECT dealership_id, vin FROM car_dealership.inventory;";
+
+        try (Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, vin);
+            statement.setInt(2, dealershipId);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void removeVehicleFromInventory(String vin) {
